e30b7e84205a5d8514154bbb376bac219841311d
Michael Karman <mkarman@mymail.mines.edu>
2015-10-06 16:49:16 -0600

    p!nk kawa!! cat w!th jaded eye

--- DrawPlay/DrawPlay/src/Cat.java
+++ DrawPlay/DrawPlay/src/Cat.java
@@ -5,14 +5,14 @@ import java.awt.geom.*;
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 200;
+	private static final int HEAD_DIMENSION = 150;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/2;
 	// pick eye dimensions
-	private static final int EYE_HEIGHT = 30;
-	private static final int EYE_WIDTH = 50;
+	private static final int EYE_HEIGHT = 35;
+	private static final int EYE_WIDTH = 20;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 10;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
@@ -27,24 +27,24 @@ public class Cat {
 		int x=catX;
 		int y=catY;
 		// Draw the head
-		g2.setColor(new Color(90,243,250));
+		g2.setColor(new Color(240,180,180));
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
-		g2.setColor(new Color(10,50,20));
+		g2.setColor(new Color(0,168,107));
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.blue);
+		g2.setColor(Color.WHITE);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.blue);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Myau", catX, catY+HEAD_DIMENSION+10);
+		g2.drawString("MyauMyauMeow", catX, catY+HEAD_DIMENSION+10);
 		g2.drawString("Im a cat :3", catX, catY+HEAD_DIMENSION+20);
 	}
 }
